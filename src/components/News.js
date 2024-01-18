0986e90b1b997567d5ec18da4c068434354d2178
bellobambo <bellobambo21@gmail.com>
2024-01-18 07:48:59 +0100

    push

--- src/components/News.js
+++ src/components/News.js
@@ -11,22 +11,24 @@ const News = () => {
 
   const displayCryptoData = cryptoData.slice(0, 20); // Display only the first 20 items
 
+  const getFormattedDate = () => {
+    const today = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return today.toLocaleDateString(undefined, options);
+  };
+
   return (
     <div>
-
-      <div className=' grid m-4 lg:grid-cols-3 '>
+      <div className='grid m-4 lg:grid-cols-3'>
         {displayCryptoData.map((crypto) => (
           <div className='m-2 border-2 rounded-md border-red-400 p-3' key={crypto.headline}>
             <h2 className='font-bold text-[20px]'>{crypto.headline}</h2>
+            <p>{crypto.content}</p>
+            <p>Date: {getFormattedDate()}</p>
 
-            <p> {crypto.content}</p>
-            <p>Date : {crypto.date}</p>
-
-            {/* Add more information as needed */}
           </div>
         ))}
       </div>
-
     </div>
   );
 };
