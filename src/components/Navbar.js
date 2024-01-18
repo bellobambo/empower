ce3ae18004b894ad5a41f616ad9c4bf52260c971
bellobambo <bellobambo21@gmail.com>
2024-01-18 07:46:37 +0100

    push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className='header  sticky bg-base-200 p-4'>
       <div className='flex justify-between items-center text-center'>
-        <img src='/logo.png' className='h-50 w-80 mt-3' alt='Logo' />
+        <img src='/logo.png' className='h-50 w-60 mt-3' alt='Logo' />
         <a className='pr-8 text-white bg-red-500 hover:text-white hover:bg-transparent btn btn-error' href="mailto:empower.investment.servicemail.co@gmail.com">
   Contact Us
 </a>
